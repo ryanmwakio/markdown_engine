f06e1c0ef7da0f5d514cba35895dbe2d3f66779d
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-01-05 13:48:48 +0300

    configured templating

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+//core imports
+const fs = require("fs");
+const path = require("path");
+
+//Third party imports
 const express = require("express");
 const bodyParser = require("body-parser");
+const showdown = require("showdown");
 
 var app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+converter = new showdown.Converter();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.post("/convert", function (req, res, next) {
-  console.log(req.body);
+app.post("/convert", (req, res, next) => {
   if (typeof req.body.content == "undefined" || req.body.content == null) {
     res.json(["error", "No data found"]);
   } else {
-    res.json(["markdown", req.body.content]);
+    text = req.body.content;
+    html = converter.makeHtml(text);
+    res.json(["markdown", html]);
   }
 });
 
-app.listen(8000, function () {
+app.listen(8000, () => {
   console.log("Server running on port 8000");
 });
