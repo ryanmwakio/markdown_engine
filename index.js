1bfdb821187e0e5e76e5b4c5058f73c176eea786
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-01-05 13:03:45 +0300

    initial api

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.post("/convert", function (req, res, next) {
+  console.log(req.body);
+  if (typeof req.body.content == "undefined" || req.body.content == null) {
+    res.json(["error", "No data found"]);
+  } else {
+    res.json(["markdown", req.body.content]);
+  }
+});
+
+app.listen(8000, function () {
+  console.log("Server running on port 3000");
+});
